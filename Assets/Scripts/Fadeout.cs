54cd56d197eca1488ef1940e1f2fb5208a0a16c1
DeusIntra <deusintra@yahoo.com>
2021-04-15 03:10:20 +0500

    level transition

---
+++ Assets/Scripts/Fadeout.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Fadeout : MonoBehaviour
+{
+    public float timeToFade = 2f;
+    public UnityEvent onFadeOut;
+
+    private Image fadeout;
+
+    private void Awake()
+    {
+        fadeout = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInCoroutine());
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeInCoroutine(false));
+    }
+
+    private IEnumerator FadeInCoroutine(bool isFadeIn = true)
+    {
+        if (isFadeIn)
+        {
+            float t = 1f;
+            while (t > 0)
+            {
+                fadeout.color = new Color(0, 0, 0, t);
+                t -= Time.deltaTime / timeToFade;
+                yield return null;
+            }
+            fadeout.color = new Color(0, 0, 0, 0);
+
+        }
+        else
+        {
+            float t = 0f;
+            while (t < 1)
+            {
+                fadeout.color = new Color(0, 0, 0, t);
+                t += Time.deltaTime / timeToFade;
+                yield return null;
+            }
+            fadeout.color = new Color(0, 0, 0, 1);
+            onFadeOut.Invoke();
+        }
+
+    }
+}
--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,16 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Teleport : MonoBehaviour
 {
+    public string nextScene;
+    public GameObject moveLocation;
+    public GameObject switchScenes;
+
     private Player player;
+    private bool isSwithScene;
 
     private void Start()
     {
         player = FindObjectOfType<Player>();
+        isSwithScene = !nextScene.Equals("");
+
+        switchScenes.SetActive(isSwithScene);
+        moveLocation.SetActive(!isSwithScene);
     }
 
     public void TeleportPlayer()
     {
         player.transform.position = transform.position;
+        if (isSwithScene) SwitchScene();
     }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void SwitchScene()
+    {
+        Fadeout fadeout = FindObjectOfType<Fadeout>();
+
+        fadeout.FadeOut();
+    }
+
+    
 }
