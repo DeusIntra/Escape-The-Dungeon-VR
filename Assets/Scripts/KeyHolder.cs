8c57bdf7e7b5c93d6fbc493f5f09010596715dab
DeusIntra <deusintra@yahoo.com>
2021-04-18 19:15:34 +0500

    dungeon main room

---
+++ Assets/Scripts/KeyHolder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHolder : MonoBehaviour
+{
+    public List<GameObject> fire;
+    public GameObject key;
+
+    private bool isFireOut = false;
+    private Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+    
+    public void PutOutFire()
+    {
+        // TODO проверка на воду
+        foreach (var obj in fire)
+        {
+            obj.SetActive(false);
+        }
+        isFireOut = true;
+    }
+
+    public void TakeKey()
+    {
+        key.SetActive(false);
+        // TODO: дать игроку ключ
+        Debug.Log("TODO");
+    }
+}
